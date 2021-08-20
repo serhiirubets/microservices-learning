dc5e8742be2c67551d30d1a743923ca0714b817b
serhiirubets <sergey.rubets@wnwd.com>
2021-08-20 10:03:45 +0300

    Finishing event-bus

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,18 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
 
-const CommentList = ({ postId }) => {
-  const [comments, setComments] = useState([]);
-
-  const fetchComments = async () => {
-    const { data } = await axios.get(`http://localhost:7001/posts/${postId}/comments`);
-    setComments(data);
-  }
-  
-  useEffect(() => {
-    fetchComments();
-  }, []);
-  
+const CommentList = ({ comments }) => {
   const renderedComments = comments.map(comment => {
     return <li key={comment.id}>{comment.content}</li>
   });
--- posts/index.js
+++ posts/index.js
@@ -3,6 +3,7 @@ const { randomBytes } = require('crypto');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
+const axios = require('axios');
 
 const posts = {};
 
@@ -13,13 +14,26 @@ app.get('/posts', (req, res) => {
   res.send(posts);
 })
 
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
   const id = randomBytes(4).toString('hex');
   const { title } = req.body;
   
   posts[id] = { id, title };
+  
+  await axios.post('http://localhost:7005/events', {
+    type: 'PostCreated',
+    data: {
+      id, title
+    }
+  });
   res.status(201).send(posts[id]);
-})
+});
+
+app.post('/events', (req, res) => {
+  console.log('Received event', req.body);
+  
+  res.send({});
+});
 
 app.listen(7000, () => {
   console.log('Listened on port 7000');
